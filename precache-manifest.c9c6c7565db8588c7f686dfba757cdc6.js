94c740cd0362b5967520e9100c03c0c1e3295ce3
Eric Kim <44147889+taehyunnkim@users.noreply.github.com>
2022-10-29 16:24:41 -0700

    Updates

---
+++ precache-manifest.c9c6c7565db8588c7f686dfba757cdc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "324d0e2d011006758dd0",
+    "url": "/disaster-live/static/css/main.d4849ef4.chunk.css"
+  },
+  {
+    "revision": "324d0e2d011006758dd0",
+    "url": "/disaster-live/static/js/main.324d0e2d.chunk.js"
+  },
+  {
+    "revision": "635f9d1c3998e930baee",
+    "url": "/disaster-live/static/css/1.378baa88.chunk.css"
+  },
+  {
+    "revision": "635f9d1c3998e930baee",
+    "url": "/disaster-live/static/js/1.635f9d1c.chunk.js"
+  },
+  {
+    "revision": "bb9fb1f8e709fd18fe41",
+    "url": "/disaster-live/static/js/runtime~main.bb9fb1f8.js"
+  },
+  {
+    "revision": "91df8de2670cbc9260861e1d93ec2556",
+    "url": "/disaster-live/index.html"
+  }
+];
